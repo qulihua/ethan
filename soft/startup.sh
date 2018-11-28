24c9b75295ca5aa91fe845c18dc719295c389db6
ethan <Ethan@Gmail.com>
2018-11-28 15:51:05 +0800

    1

--- soft/startup.sh
+++ soft/startup.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 echo starting
 java -Xms2400m -Xmx2400m -jar collector.jar --spring.config.location=/data/collector/application.properties > /dev/null 2>&1 &
+
+java   -XX:-OmitStackTraceInFastThrow -Xms2400m -Xmx2400m -jar swan.jar --spring.config.location=/opt/lottery/swan/application.properties > log.file 2>&1 &
+
